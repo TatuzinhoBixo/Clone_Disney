c724d58f2f340e21b440153002dabc07fa01d52b
Tatuzao <celuw_lima@hotmail.com>
2024-04-21 17:42:41 -0400

    vai

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
+//const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts () {
@@ -15,13 +15,13 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 }
 
-function images() {
-    return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
-}
+//function images() {
+//    return gulp.src('./src/images/**/*')
+//    .pipe(imagemin())
+//    .pipe(gulp.dest('./dist/images'));
+//}
 
-exports.default = gulp.parallel(styles, images, scripts);
+exports.default = gulp.parallel(styles, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
