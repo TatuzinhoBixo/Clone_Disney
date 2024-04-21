e19835deae4ba858a535f6c958abd5b3a22a5775
Tatuzao <celuw_lima@hotmail.com>
2024-04-21 16:52:58 -0400

    final mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const auturaHero = heroSection.clientHeight;
     
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < auturaHero) {
+            ocultaElementoDoHeader();
+        } else {
+            exibeElementoDoHeader();
+        }
+        
+    })
+
+    //seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
             buttons[i].addEventListener('click',function(botao) {
                 const abaAlvo = botao.target.dataset.tabButton;
@@ -12,7 +28,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
         })
     }
+    
+    //seção de perguntas frequentes, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta); 
+
+    }
+
 })
+
+function ocultaElementoDoHeader() {
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementoDoHeader() {
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    console.log(elemento);
+    const elementoPai = elemento.target.parentElement;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
